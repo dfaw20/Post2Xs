bdc18e3bfc4a2d5188eb36d35a696fbd698f19f0
dfaw20 <dfaw200@gmail.com>
2023-09-10 01:06:55 +0900

    init

---
+++ background.js
@@ -0,0 +1,12 @@
+// background.js
+
+// アクションボタンがクリックされたときにポップアップを表示
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: () => {
+        // ページ上のテキストを置換するコードを実行
+        replaceText();
+      },
+    });
+  });
---
+++ content.js
@@ -0,0 +1,34 @@
+// content.js
+
+// ページ読み込み後にテキストを置換
+replaceText();
+
+// ページの変更を監視して、動的にテキストを置換
+const observer = new MutationObserver(replaceText);
+observer.observe(document.body, { subtree: true, characterData: true, childList: true });
+
+// ページ上のテキストを"ポスト"から"X's"に置換する関数
+function replaceText() {
+  const elements = document.querySelector(`[data-testid="tweetButtonInline"]`);
+
+  if (elements) {
+    // 要素内の入れ子構造をトラバースし、目当てのテキストノードを取得
+    let textNode;
+    const traverse = (node) => {
+      if (node.nodeType === Node.TEXT_NODE) {
+        textNode = node;
+      } else {
+        for (const childNode of node.childNodes) {
+          traverse(childNode);
+        }
+      }
+    };
+    traverse(elements);
+  
+    if (textNode) {
+      // テキストノードの内容を書き換え
+      textNode.textContent = textNode.textContent.replace(/ポスト/g, "X's");
+    }
+  }
+}
+  
